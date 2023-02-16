a93af25f7f6fad879193d9e04b96a8fb8cd0330c
Johan Valentino Ghinelli <116793076+jj-sium@users.noreply.github.com>
2023-02-16 10:34:15 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ghinelli.johan._4h.gelati.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ghinelli.johan._4h.gelati
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string nomefile = "db.csv";
+                Gelati gg = new Gelati(nomefile);
+                dbDati.ItemsSource = gg;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+    }
+}
